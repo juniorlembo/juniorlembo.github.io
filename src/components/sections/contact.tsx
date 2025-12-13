63b34c09948c2c247c19df1b1e15df1d92803a60
Ilem Isaiah Nelson Junior LEMBO <110841264+IlemLembo@users.noreply.github.com>
2025-12-13 18:48:16 +0000

    added

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -64,7 +64,7 @@ export function Contact() {
             </div>
           <div className="mx-auto w-full max-w-md">
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4" netlify>
               <FormField
                 control={form.control}
                 name="name"
